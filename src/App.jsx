2211cc2203a9ad8b10c717fd30ac238976c3a357
wanying <wanying@qq.com>
2019-07-14 22:16:53 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React,{Component} from 'react'
+import {BrowserRouter,Switch,Route,} from 'react-router-dom'
+
+import Admin from './gages/admin/admin'
+import Login from './gages/login/login'
+
+
+
+export default class App extends Component{
+    render(){
+        return(
+            <BrowserRouter>
+                <Switch>
+                     <Route path="/admin" component={Admin}/>
+                    <Route path='/' component={Login}/>
+                </Switch>
+            </BrowserRouter>
+            
+           
+        )
+        
+    }
+}
